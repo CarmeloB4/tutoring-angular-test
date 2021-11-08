361352334c17b4918cdf271d0ecc81bff2218a5d
Melo <melonemark8@gmail.com>
2021-11-08 15:10:41 +0100

    Create components and folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './modules/core/container/container.component';
+import { ListComponent } from './modules/core/components/list/list.component';
+import { CardComponent } from './modules/core/components/card/card.component';
+import { DetailsComponent } from './modules/core/components/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    ListComponent,
+    CardComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
